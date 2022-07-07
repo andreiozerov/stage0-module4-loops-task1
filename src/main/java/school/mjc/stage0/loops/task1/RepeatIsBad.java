7a9f8d1e9a99893982b246a3cc94d25b9e79bcaa
Andrei Ozerov <andrei.ozerov@gmail.com>
2022-07-07 14:04:06 +0300

    Introduction + Task #1. Repeat Is Bad Done

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,7 +1,31 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
-    public void repeatIsBad(){
+    public static final String TEXT = "writing the same code doesn't have much impact, and it's also time consuming";
 
+    public void repeatIsBad() {
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
+        System.out.println(TEXT);
     }
 }
